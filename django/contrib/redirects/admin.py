ba69685eb09ab41f38358e96671b7836f74aea13
Dmitry Pechnikov <d.pechnikov@semrush.com>
2017-09-04 12:47:36 +0300

    wip: override empty domain field with site domain if sites framework is installed

--- django/contrib/redirects/admin.py
+++ django/contrib/redirects/admin.py
@@ -10,20 +10,33 @@ from django.utils.translation import gettext_lazy as _
 
 
 class RedirectForm(forms.ModelForm):
-    if apps.is_installed('django.contrib.sites'):
-        from django.contrib.sites.models import Site
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-        site = forms.ModelChoiceField(
-            queryset=Site.objects.all(),
-            to_field_name='domain',
-            empty_label='Any',
-            widget=widgets.AdminRadioSelect(attrs={
-                'class': get_ul_class(admin.VERTICAL),
-            }),
-            label=_('Site'),
-            help_text=_('If domain is not set, redirect from this site.'),
-            required=False,
-        )
+        if apps.is_installed('django.contrib.sites'):
+            from django.contrib.sites.models import Site
+
+            self.fields['site'] = forms.ModelChoiceField(
+                queryset=Site.objects.all(),
+                to_field_name='domain',
+                empty_label='Any',
+                widget=widgets.AdminRadioSelect(attrs={
+                    'class': get_ul_class(admin.VERTICAL),
+                }),
+                label=_('Site'),
+                help_text=_('If domain is not set, redirect from this site.'),
+                required=False,
+            )
+
+    def save(self, commit=True):
+        self.full_clean()
+
+        if apps.is_installed('django.contrib.sites'):
+            site = self.cleaned_data.get('site')
+
+            self.instance.domain = self.instance.domain or getattr(site, 'domain', '')
+
+        return super().save(commit=commit)
 
 
 @admin.register(Redirect)
